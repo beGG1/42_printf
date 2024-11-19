04b004d93884dce2e5dbd2df99d79d5707b228bd
Stepan Shabalinov <shabalinov2341234@gmail.com>
2024-11-20 02:58:34 +0300

    start of libft project

---
+++ includes/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sshabali <sshabali@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 02:16:53 by sshabali          #+#    #+#             */
+/*   Updated: 2024/11/20 02:16:56 by sshabali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
---
+++ main.c
@@ -0,0 +1,5 @@
+# include "includes/ft_printf.h"
+
+int main() {
+    ft_printf("avc");
+}
